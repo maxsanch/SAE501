5a110c4ed63427f2feeebd5ace952ef876b99c3c
Maxence_Sanchez <maxence.sanchez-varas-leclercq@uha.fr>
2025-11-18 16:47:57 +0100

    push

--- predict_order.py
+++ predict_order.py
@@ -12,40 +12,48 @@ if len(sys.argv) < 2:
     print(json.dumps({"probability": 0}))
     sys.exit(0)
 
+    
 try:
     data = json.loads(sys.argv[1])
 except json.JSONDecodeError:
     print(json.dumps({"probability": 0}))
     sys.exit(0)
 
-# Conversion sexe
-sexegood = data.get('sexe')
 
-# Construire le dictionnaire des features
-features = {
-    'sexe': sexegood,
-    'age': float(data.get('age')),
-    'intensite': float(data.get('intensite')),
-    'prix': float(data.get('prix')),
-}
-
+client = data.get('client', {})
+products = data.get('products', [])
 # Gestion catégories et historique (one-hot)
 cat_columns = ['cat_Amour', 'cat_Apaisant', 'cat_Fantaisie', 'cat_Historique', 'cat_Horreur', 'cat_SF']  # exemple, adapter à tes colonnes
 hist_columns = ['hist_Amour','hist_Apaisant','hist_Fantaisie','hist_Historique','hist_Horreur','hist_None','hist_SF']  # exemple
+results = []
 
-categorie = data.get('categorie', 'None')
-historique = data.get('historique', 'None')
+# Construire le dictionnaire des features
+for p in products:
+    features = {
+        'sexe': client.get('sexe', 1),
+        'age': float(client.get('age', 25)),
+        'intensite': float(p.get('intensite', 0)),
+        'prix': float(p.get('prix', 0)),
+    }
 
-for col in cat_columns:
-    features[col] = 1 if col == f'cat_{categorie}' else 0
-for col in hist_columns:
-    features[col] = 1 if col == f'hist_{historique}' else 0
+    categorie = data.get('categorie', 'None')
+    historique = data.get('historique', 'None')
 
-# Transformer en DataFrame
-final_features = pd.DataFrame([features])
+    for col in cat_columns:
+        features[col] = 1 if col == f'cat_{categorie}' else 0
+    for col in hist_columns:
+        features[col] = 1 if col == f'hist_{historique}' else 0
 
-# Prédiction
-proba = model.predict_proba(final_features)[0][1]
+    # Transformer en DataFrame
+    final_features = pd.DataFrame([features])
+
+    # Prédiction
+    proba = model.predict_proba(final_features)[0][1]
+    
+    results.append({
+        "id": p.get('id'),
+        "probability": float(proba)
+    })
 
 # Retourner au PHP
-print(json.dumps({"probability": round(proba, 2)}))
+print(json.dumps(results))
